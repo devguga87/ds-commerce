fd3a0b5207e6f5112a0f0cd25867d7ce49bc216a
Gustavo Seabra <devguga87@gmail.com>
2025-07-20 11:03:07 -0300

    feat: erros validacao

--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.devsuperior.dscommerce.repositories;
 
 import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    default Product getById(Long id) {
+        return findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+    }
 }
