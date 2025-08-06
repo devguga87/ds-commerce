a0140d6d38f06a071303d17cd583a760406ecb60
Gustavo Seabra <devguga87@gmail.com>
2025-08-06 18:06:01 -0300

    adicionar config e ajusta arquivos

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -7,16 +7,18 @@ import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
+    @Override
     public UserDetails loadUserByUsername(String username){
         List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
         if(result.size() == 0){
